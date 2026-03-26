8378e577f0036b7054621ab701e3e4d471451842
German Bayon <desarrollosoftwarebim@gmail.com>
2026-03-26 17:39:48 +0100

    chore: autorizar dominio marebo.b-cdn.net en next.config

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,12 @@ const nextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'marebo.b-cdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
